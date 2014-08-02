2bb43fce9d41041d564a90979d65a3793db2bd3a
Cameron Currie <me@cameroncurrie.net>
2014-08-02 15:25:44 -0500

    Add button to app launcher to show/hide controls

--- kTunes/kTunes.cs
+++ kTunes/kTunes.cs
@@ -10,56 +10,86 @@ using WindowsInput;
 
 namespace kTunes
 {
-	[KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+	[KSPAddon (KSPAddon.Startup.SpaceCentre, true)]
 	public class kTunes : MonoBehaviour
 	{
+		public const string Name = "kTunes";
 		public static GameObject Instance;
-		private static Rect playerWindow = new Rect(50, 50, 300, 50);
-		private static int playerWindowId = UnityEngine.Random.Range(101, int.MaxValue); // Use a random window ID so a conflict will be unlikely
 
-		public void Awake()
+		// Use a random window ID so a conflict will be unlikely
+		private static int playerWindowId = UnityEngine.Random.Range (500, int.MaxValue);
+		private static Rect playerWindow = new Rect (50, 50, 300, 50);
+
+		private static ApplicationLauncherButton appLauncherButton;
+		private static bool isPlayerVisible = false;
+
+		public void Awake ()
 		{
-			DontDestroyOnLoad(this);
+			DontDestroyOnLoad (this);
+
+			GameEvents.onGUIApplicationLauncherReady.Add (OnGUIApplicationLauncherReady);
 		}
 
-		public void OnGUI()
+		private void OnGUIApplicationLauncherReady ()
 		{
-			if (ShouldDrawGui)
-			{
-				playerWindow = GUI.Window(playerWindowId, playerWindow, OnLayoutPlayerWindow, "kTunes", HighLogic.Skin.window);
+			appLauncherButton = ApplicationLauncher.Instance.AddModApplication (
+				OnAppLauncherToggleOn,
+				OnAppLauncherToggleOff,
+				NoOp,
+				NoOp,
+				NoOp,
+				NoOp,
+				ApplicationLauncher.AppScenes.ALWAYS,
+				(Texture)GameDatabase.Instance.GetTexture (Name + "/Textures/app_launcher_icon", false));
+		}
+
+		private void OnAppLauncherToggleOn ()
+		{
+			isPlayerVisible = true;
+		}
+
+		private void OnAppLauncherToggleOff ()
+		{
+			isPlayerVisible = false;
+		}
+
+		private void NoOp ()
+		{
+		}
+
+		public void OnGUI ()
+		{
+			if (isPlayerVisible) {
+				playerWindow = GUI.Window (playerWindowId, playerWindow, OnLayoutPlayerWindow, Name, HighLogic.Skin.window);
 			}
 		}
 
-		private void OnLayoutPlayerWindow(int id)
+		private void OnLayoutPlayerWindow (int id)
 		{
-			GUILayout.BeginHorizontal();
+			GUILayout.BeginHorizontal ();
 
-			if (GUILayout.Button("P"))
-			{
-				InputSimulator.SimulateKeyPress(VirtualKeyCode.MEDIA_PREV_TRACK);
+			if (GUILayout.Button ("P")) {
+				InputSimulator.SimulateKeyPress (VirtualKeyCode.MEDIA_PREV_TRACK);
 			}
 
-			if (GUILayout.Button("P/P"))
-			{
-				InputSimulator.SimulateKeyPress(VirtualKeyCode.MEDIA_PLAY_PAUSE);
+			if (GUILayout.Button ("P/P")) {
+				InputSimulator.SimulateKeyPress (VirtualKeyCode.MEDIA_PLAY_PAUSE);
 			}
 
-			if (GUILayout.Button("N"))
-			{
-				InputSimulator.SimulateKeyPress(VirtualKeyCode.MEDIA_NEXT_TRACK);
+			if (GUILayout.Button ("N")) {
+				InputSimulator.SimulateKeyPress (VirtualKeyCode.MEDIA_NEXT_TRACK);
 			}
 
-			GUILayout.EndHorizontal();
+			GUILayout.EndHorizontal ();
 
-			GUI.DragWindow();
+			GUI.DragWindow ();
 		}
 
-		public bool ShouldDrawGui
+		public void OnDestroy ()
 		{
-            get
-            {
-                return true;
-            }
+			GameEvents.onGUIApplicationLauncherReady.Remove (OnGUIApplicationLauncherReady);
+			if (appLauncherButton != null)
+				ApplicationLauncher.Instance.RemoveModApplication (appLauncherButton);
 		}
 	}
 }
